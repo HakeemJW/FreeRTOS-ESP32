e00bba11c0e514e743ea01b86b43261d9643cc81
HakeemJW <wilsonhakeem@gmail.com>
2022-08-20 03:14:58 -0400

    completed interrupt service routine challenge

--- InterruptServiceRoutineChallenge/InterruptServiceRoutineChallenge.ino
+++ InterruptServiceRoutineChallenge/InterruptServiceRoutineChallenge.ino
@@ -7,11 +7,15 @@
 // Settings
 static const uint16_t timer_divider = 8; // Clock ticks at 10 MHz
 static const uint64_t timer_max_count = 1000000;
-static const TickType_t task_delay = 2000 / portTICK_PERIOD_MS;
+static const TickType_t task_delay = 1000 / portTICK_PERIOD_MS;
+static const TickType_t read_delay = 20 / portTICK_PERIOD_MS;
 static const uint8_t buf_len = 255;
+enum {BUF_SIZE = 10};
+static const int max_samples = 10;
 
 // Pins
 static const int led_pin = LED_BUILTIN;
+static const int adc_pin = A0;
 
 // Globals
 static hw_timer_t * timer = NULL;
@@ -19,7 +23,15 @@ static volatile int isr_counter;
 static portMUX_TYPE spinlock = portMUX_INITIALIZER_UNLOCKED;
 static volatile uint8_t msg_flag = 0;
 static char *msg_ptr = NULL;
-static volatile float adc_avg;
+static float adc_avg = 0;
+static volatile uint16_t val;
+static int total = 0;
+static int buf[BUF_SIZE];
+static int head = 0;
+static int tail = 0;
+static SemaphoreHandle_t fill_sem;
+static SemaphoreHandle_t empty_sem;
+static SemaphoreHandle_t mutex;
 
 //*****************************************************************
 // Interrupt Service Routines
@@ -27,46 +39,52 @@ static volatile float adc_avg;
 // This function executes when timer reaches max (and resets)
 void IRAM_ATTR onTimer()
 {
-  // ESP-IDF version of a critical section (in an ISR)
-  portENTER_CRITICAL_ISR(&spinlock);
-  isr_counter++;
-  portEXIT_CRITICAL_ISR(&spinlock);
-
-  // Vanilla FreeRTOS version
-  //UBaseType_t saved_int_status;
-  //saved_int_status = taskENTER_CRITICAL_FROM_ISR();
-  //isr_counter++;
-  //taskEXIT_CRITICAL_FROM_ISR(saved_int_status);
+  BaseType_t task_woken = pdFALSE;
+  
+  //xSemaphoreTakeFromISR(empty_sem, &task_woken);
+  // Perform action (read from ADC)
+  val = analogRead(adc_pin);
+  buf[head] = val;
+  head = (head + 1) % BUF_SIZE;
+  
+  // Give semaphore to tell task that new value is ready
+  xSemaphoreGiveFromISR(fill_sem, &task_woken);
+  
+  // Exit from ISR (ESP-IDF)
+  if (task_woken) {
+    portYIELD_FROM_ISR();
+  }
 }
 
 //*****************************************************************************
 // Tasks
 
 // Wait for semaphore and print out ADC value when received
-void printValues(void *parameters) {
+void computeAverage(void *parameters) {
 
+  int count = 0;
   // Loop forever
   while (1) {
     
-    // Count down and print out counter value
-    while (isr_counter > 0) {
+    // Wait for at least one slot in buffer to be filled
+    xSemaphoreTake(fill_sem,portMAX_DELAY);
 
-      // Print value of counter
-      Serial.println(isr_counter);
-  
-      // ESP-IDF version of a critical section (in a task)
-      portENTER_CRITICAL(&spinlock);
-      isr_counter--;
-      portEXIT_CRITICAL(&spinlock);
-
-      // Vanilla FreeRTOS version of a critical section (in a task)
-      //taskENTER_CRITICAL();
-      //isr_counter--;
-      //taskEXIT_CRITICAL();
+    // Take the mutex to  lock critical section
+    xSemaphoreTake(mutex, portMAX_DELAY);
+    total += buf[tail];
+    tail = (tail + 1) % BUF_SIZE;
+    count++;
+    
+    if (count == 9 ) 
+    {
+      adc_avg = total/max_samples;
+      count = 0;
+      total = 0;
     }
-  
-    // Wait 2 seconds while ISR increments counter a few times
-    vTaskDelay(task_delay);
+    xSemaphoreGive(mutex);
+
+    // Signal to producer thread that a slot in the buffer is free
+    xSemaphoreGive(empty_sem);
   }
 }
 
@@ -115,12 +133,29 @@ void readSerial(void *parameters) {
           //digitalWrite(led_pin, HIGH);
         }
 
+        if (msg_flag == 1) 
+        {
+          Serial.print("You said: ");
+          Serial.println(msg_ptr);
+
+          if (strcmp(msg_ptr,"avg") == 0)
+          {
+            Serial.print("Average: ");
+            Serial.println(adc_avg);
+          }
+          vPortFree(msg_ptr);
+          msg_ptr = NULL;
+          msg_flag = 0;
+        }
+          
         // Reset receive buffer and index counter
         memset(buf, 0, buf_len);
         idx = 0;
       } 
     }
   }
+  // Wait 1 seconds while ISR increments counter a few times
+  vTaskDelay(read_delay);
 }
 
 
@@ -133,14 +168,29 @@ void setup() {
   Serial.println();
   Serial.println("---FreeRTOS ISR Critical Section Demo---");
 
+  // Create mutex before starting tasks
+  mutex = xSemaphoreCreateMutex();
+
+  // Create semaphore (start at zero)
+  fill_sem  = xSemaphoreCreateCounting(BUF_SIZE,0);
+  empty_sem = xSemaphoreCreateCounting(BUF_SIZE,BUF_SIZE);
+
   // Start task to print out results
-  xTaskCreatePinnedToCore(printValues,
-                          "Print values",
-                          1024,
+  xTaskCreatePinnedToCore(readSerial,
+                          "readSerial",
+                          2000,
                           NULL,
                           1,
                           NULL,
                           app_cpu);
+    
+  xTaskCreatePinnedToCore(computeAverage,
+                        "computeAverage",
+                        1024,
+                        NULL,
+                        1,
+                        NULL,
+                        app_cpu);
 
   // Create and start timer (num,divider,countUP)
   timer = timerBegin(0, timer_divider, true);
@@ -154,6 +204,8 @@ void setup() {
   // Allow ISR to trigger
   timerAlarmEnable(timer);
 
+  // Delete "setup and loop" task
+  vTaskDelete(NULL);
 }
 
 void loop() {
